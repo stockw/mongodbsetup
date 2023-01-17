5682bfa6d57832718e25771e429ee4fd0c680aae
stockw <courtney.wilkins05@gmail.com>
2023-01-17 16:42:51 -0500

    get/remove data from database

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 console.log("js file connected");
 
 
@@ -13,28 +15,73 @@ submitButton.addEventListener('click', async () => {
       // let nameString = document.getElementById('name;-input')
       // // get value
       // let nameString = nameElement.value;
-      let nameString = document.getElementById('name=input').value;
+      let nameString = document.getElementById('name-input').value;
       let colorString = document.getElementById('color-input').value;
       let ageNumber = +document.getElementById('age-input').value;
+       // using ternary operator here - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
       let readyBool = document.getElementById('ready-bool').value === "true" ? true : false;
 
+       // packing all our data in an object
+      // same as 
+      // nameString: nameString
       const fruit = {
          nameString,
          colorString,
          ageNumber,
-         readyBool: typeof readyBool
+         readyBool
       }
 
-      console.log(json.stringify(fruit));
+      // console.log(JSON.stringify(fruit));
 
-      fetch('http://localhost:5000/create_fruit', {
+      let response = await fetch('http://localhost:5000/create_fruit', {
          method: "POST",
          headers: {'Content-Type': 'application/json',
          },
-         body: json.stringify(fruit)
-        
+          // to send JSON data over HTTP
+         body: JSON.stringify(fruit)  
       })
+      let uploadStatusTag = document.getElementById('upload=status');
+      console.log(response.status);
+      if (response.status === 200) {
+         console.log(response);
+         console.log("upload complete!");
+         uploadStatusTag.textContent = "Upload Complete";
+         uploadStatusTag.style.color = "green"
 
+      } else {
+         console.log(response);
+         console.log("upload failed");
+         console.log();
+         uploadStatusTag.textContent = "Upload Failed";
+         uploadStatusTag.style.color = "red";
+      }
+      
+      // axios({
+   //    method: "post",
+   //    url: "'http://localhost:5000/create_fruit",
+   //    bosy: fruit
+   // })
+
+   })
+
+
+let deleteButton = document.getElementById('delete');
+
+deleteButton.addEventListener('click', async () => {
+   let response = await fetch ('http://localhost:5000/delete_nameless_data', {
+      method: "delete",
+   });
+   //console.log(response);
+
+let parsedData = await response.json ()
+console.log(parsedData);
+});
+
+let displayPageButton = document.getElementById('display-page-button');
+
+displayPageButton.addEventListener('click', () => {
+   // change HTML files (from index to display_food.html)
+   window.location.href = './display_food'
 })
 
 
--- server.js
+++ server.js
@@ -1,23 +1,46 @@
 
 const express = require('express')
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+// allows us to use information from .env in this file
 require('dotenv').config()
+
+// import MyFruit object from fruit.js
 const MyFruits = require('./models/fruits')
+
+// create app by calling express function
 const app = express()
 
-
+// tells express to serve our public folder by default when someone makes a request to this port
 app.use(express.static('public'))
+
+// parses (makes readable) string JSON back into actual objects found in req.body
 app.use(express.json())
+
+// allow use of queries in URL (?limit=2&color=green) 
+// extended allows nested objects in URL
 app.use(express.urlencoded({extended:true}));
 
+app.use(cors({
+  origin: '*'
+}));
 
+
+
+// string we get from MongoDB - we hide our username and password in our .env file
 let connectionString = `mongodb+srv://${process.env.MONGOUSERNAME}:${process.env.MONGOPASSWORD}@mongosetupcluster.1dnn4wp.mongodb.net/fruits?retryWrites=true&w=majority`;
+
+// by default mongoose 'strictQuery' is true (strict) meaning we cant ask for information not in our schema
+// see more here: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
 mongoose.set('strictQuery', false);
+// connect to our MongoDB database (our Models specify which collections)
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true
   });
+  // function will activate once to let us know we are connected
   mongoose.connection.once('open', ()=> {
     console.log('connected to mongo');
   });
@@ -27,39 +50,56 @@ mongoose.connect(connectionString, {
 // before I can ask and send data into the collection, I need to create a model
 
 app.post('/create_fruit', async (req,res) => {
-    
+        // destructuring - see more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    // renaming variable while destrucutring: https://wesbos.com/destructuring-renaming
     const {nameString: name, colerString: color, ageNumber: age, readyBool: readyToEat} = req.body;
     
 
-    // // console.log("uploading to database...");
+    // console.log("uploading to database...");
+    // Model methods usually give us a promise, so we can wait for the response
     let returnedValue = await MyFruits.create({
        name,
        color,
        age,
        readyToEat
     })
+   
     console.log(returnedValue);
     if (returnedValue) {
         console.log("upload complete");
     }
+    res.status(400);
     res.send(returnedValue);
-    // {
-    //     name: "apple",
-    //     color: "red",
-    //     age: 14,
-    //     readyToEat: true
-    //   })
-      res.send("good request")
 })
-app.get('/get_data', (req,res) => {
-    // Get data from MongoDB
-    // res.json(data)
-    res.setHeader('Content-Type', 'application/json');
 
-    console.log("request received at /get_data");
-    console.log(process.env.Kingcourt86);
-    res.json({data: "response from server"})
+// app.get('/get_data', (req,res) => {
+//     // Get data from MongoDB
+//     // res.json(data)
+//     res.setHeader('Content-Type', 'application/json');
+
+//     console.log("request received at /get_data");
+//     console.log(process.env.Kingcourt86);
+//     res.json({data: "response from server"})
+// })
+
+app.delete("/delete_nameless_data", async (reg, res) => {
+  let response = await MyFruits.deleteMany({name: ""});
+  
+  console.log(response);
+
+  res.send({data: `deleted ${response.deletedCount} items.`})
 })
+
+app.get('/get_food_data', async (req, res) => {
+  // get data from database
+  let response = await MyFruits.find({});
+  console.log(response);
+  // send it back to front end
+  res.json(response)
+
+});
+
+
 
 app.listen(5000, () => {
     console.log(`Server is Listening on 5000`)
